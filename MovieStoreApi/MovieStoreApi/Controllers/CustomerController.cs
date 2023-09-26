808dbbe48648f66cb13761d3787aacf6e2653b79
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-09-26 12:11:54 +0200

    Made RESTful controllers

---
+++ MovieStoreApi/MovieStoreApi/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreCore.Domain;
+using MovieStoreCore.Domain.Enums;
+
+namespace MovieStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly List<Customer> _customers = new List<Customer>();
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            return Ok(_customers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(Guid id)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid();
+            _customers.Add(customer);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCustomer(Guid id, Customer newCustomer)
+        {
+            Customer? oldCustomer = _customers.Find(c => c.Id == id);
+            if (oldCustomer == null)
+            {
+                return NotFound();
+            }
+            oldCustomer.StatusExpirationDate = newCustomer.StatusExpirationDate;
+            oldCustomer.Status = newCustomer.Status;
+            oldCustomer.Email = newCustomer.Email;
+            oldCustomer.Role = newCustomer.Role;
+            return NoContent();
+        }
+
+        [HttpPut("promote/{id}")]
+        public IActionResult PromoteCustomer(Guid id)
+        {
+            Customer? oldCustomer = _customers.Find(c => c.Id == id);
+            if (oldCustomer == null)
+            {
+                return NotFound();
+            }
+            oldCustomer.StatusExpirationDate = DateTime.Now.AddYears(1);
+            oldCustomer.Status = Status.Advanced;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(Guid id)
+        {
+            Customer? customer = _customers.Find(c => c.Id == id);
+            if (customer != null)
+            {
+                _customers.Remove(customer);
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpPost("purchase")]
+        public IActionResult PurchaseMovie(Customer customer, [FromBody] Movie movie)
+        {
+            PurchasedMovie purchasedMovie = new PurchasedMovie
+            {
+                Id = Guid.NewGuid(),
+                PurchaseDate = DateTime.Now,
+                ExpirationDate = DateTime.Now.AddYears(1),
+                Customer = customer,
+                Movie = movie
+            };
+
+            // Process the purchasedMovie object as needed
+            return NoContent();
+        }
+    }
+}
+
+
---
+++ MovieStoreApi/MovieStoreApi/Controllers/MovieController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreCore.Domain;
+
+namespace MovieStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly List<Movie> _movies = new List<Movie>();
+
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            return Ok(_movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(Guid id)
+        {
+            var movie = _movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie([FromBody] Movie movie)
+        {
+            movie.Id = Guid.NewGuid();
+            _movies.Add(movie);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMovie(Guid id, [FromBody] Movie newMovie)
+        {
+            Movie oldMovie = _movies.Find(m => m.Id == id);
+            if (oldMovie == null)
+            {
+                return NotFound();
+            }
+            oldMovie.Title = newMovie.Title;
+            oldMovie.Year = newMovie.Year;
+            oldMovie.LicensingType = newMovie.LicensingType;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(Guid id)
+        {
+            Movie movie = _movies.Find(m => m.Id == id);
+            if (movie != null)
+            {
+                _movies.Remove(movie);
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+}
+
